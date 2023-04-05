8c7133b8041c4b6f7a91c0f7dffb30db2d33a201
Maksim Akimov <79629719220@ya.ru>
2023-04-06 00:58:26 +0300

    Сформирована базовая структура проекта

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles/lk-competencies.css';
+import './styles/lk-personal-data.css';
+import './styles/lk-portfolio.css';
+import './styles/lk-social.css';
+import './styles/registration.css';
+import './styles/registration-first-step.css';
+import './styles/registration-new-volunteer.css';
+import './styles/settings-change-password.css';
+import './styles/settings-notifications.css';
+import './styles/settings-profile.css';
