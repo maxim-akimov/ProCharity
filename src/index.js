dc6896a31060f7658a0414c7969c5caa62a19166
maxim-akimov <79629719220@ya.ru>
2023-04-21 15:12:32 +0300

    Реализован функционал очистки значений текстовых полей

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+// Подключение стилей
 import './styles/layout.css';
 import './styles/content.css';
 import 'cropperjs/dist/cropper.css'
+
+
+// Подключение компонентов проекта
 import {handleTextareaAutosize} from './components/textarea-autosize';
 import {handleTextareaSymbolCounter} from './components/textarea-symbol-counter';
 import {setFilesRemover} from './components/uploader-file-remover';
+import FieldTextCleaner from './components/FieldTextCleaner';
+
 import CustomSelect from "./components/CustomSelect";
 import CustomMultiselect from "./components/CustomMultiselect";
 import Popup from './components/Popup';
+
+
+// Подключение сторонних библиотек
 import 'cropperjs';
 import Cropper from 'cropperjs';
 
+const avatar = document.querySelector('.avatar__container:has(.avatar__img)');
 const image = document.querySelector('.popup__image');
+const popup = new Popup('.popup');
+const inputs = document.querySelectorAll('.input, .textarea');
 
+
+// Инициализация библиотеки Cropperjs (обрезка изображений)
 if (image) {
   const cropper = new Cropper(image, {
     aspectRatio: 1,
@@ -20,8 +34,7 @@ if (image) {
 }
 
 
-// Автоматическое увеличение высоты поля
-// от количества введенного текста
+
 handleTextareaAutosize();
 
 
@@ -31,17 +44,34 @@ handleTextareaSymbolCounter();
 setFilesRemover();
 
 
+// Инициализация кастомного выпадающего списка для поля
+// "Предпочтительный способ связи"
 new CustomSelect('#connection').generate();
+
+
+// Инициализация кастомного выпадающего списка для поля
+// "Название компании"
 new CustomSelect('#companyName').generate();
+
+
+// Инициализация кастомного дыухуровнего выпадающего списка для поля
+// "Выбор компетенций"
 new CustomMultiselect('#competencies').generate();
 
-const popup = new Popup('.popup');
+
+// Подключение класса сброса значений полей формы
+if (inputs && inputs.length > 0) {
+  inputs.forEach((input) => {
+    new FieldTextCleaner(input).setEventListeners();
+  })
+}
+
+
+// Обеспечение работы модальных окон
 popup.setEventListeners();
 
-const avatarImg = document.querySelector('.avatar:has(.avatar__img)');
-
-if (avatarImg) {
-  avatarImg.addEventListener('mousedown', () => {
+if (avatar) {
+  avatar.addEventListener('mousedown', () => {
     popup.open();
   });
 }
