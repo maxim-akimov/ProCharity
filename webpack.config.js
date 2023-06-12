92161e8873db0df0788b60bae31fd165b2df20e9
Nikolai Glebov <vdk.fire@mail.ru>
2023-06-12 13:19:38 +0200

    Добавлена страница «Личный кабинет НКО / Документы и файлы»

--- webpack.config.js
+++ webpack.config.js
@@ -115,6 +115,11 @@ module.exports = {
             template: './src/registration-documents.html',
             inject: 'body'
         }),
+        new HtmlWebpackPlugin({
+            filename: 'account-documents.html',
+            template: './src/account-documents.html',
+            inject: 'body'
+        }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin()
     ]
