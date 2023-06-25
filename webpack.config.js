d6d254da87afad33fe7932d3f215a557c663ee1e
Eugeny Ptichkin <eugeny.ptichkin@novsu.ru>
2023-06-25 16:53:15 +0300

    EAP: добавил отображение страницы fund/hours в index.html webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -128,7 +128,11 @@ module.exports = {
             template: "./src/evaluation-of-volunteer-review.html",
             inject: "body",
         }),
-
+		new HtmlWebpackPlugin({
+            filename: "evaluation-of-the-funds-work-labor-costs.html",
+            template: "./src/evaluation-of-the-funds-work-labor-costs.html",
+            inject: "body",
+        }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin(),
     ],
