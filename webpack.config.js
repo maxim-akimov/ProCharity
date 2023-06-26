22eca5ab6950d8887116cda53a644ab1f2ca7ffc
GolotinaNina <102403588+GolotinaNina@users.noreply.github.com>
2023-06-26 17:30:29 +0300

    добавлены ссылки в webpack

--- webpack.config.js
+++ webpack.config.js
@@ -114,7 +114,32 @@ module.exports = {
             inject: "body",
         }),
 
+        new HtmlWebpackPlugin({
+            filename: "estimation-work-volonter.html",
+            template: "./src/estimation-work-volonter.html",
+            inject: "body",
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "evaluation-of-volunteer-rate.html",
+            template: "./src/evaluation-of-volunteer-rate.html",
+            inject: "body",
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "rate_fund-review.html",
+            template: "./src/rate_fund-review.html",
+            inject: "body",
+        }), 
+
+        new HtmlWebpackPlugin({
+            filename: "evaluation-of-the-funds-work-labor-costs.html",
+            template: "./src/evaluation-of-the-funds-work-labor-costs.html",
+            inject: "body",
+        }), 
+
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin(),
     ]
 }
+
